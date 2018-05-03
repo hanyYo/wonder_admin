b9589043663b37ea83c8fa69e16876b8c932df30
yjw <yjw@wemakeprice.com>
2018-05-03 16:37:32 +0900

    css minify js/lib

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,14 @@ var uglify = require('gulp-uglify');
 var minifyhtml = require('gulp-minify-html');
 var sass = require('gulp-sass');
 var livereload = require('gulp-livereload');
+var cleancss = require('gulp-clean-css');
 
 var src = 'public/src';
 var dist = 'public/dist';
 
 var paths = {
 	js: src + '/js/*.js',
+	js_lib: src + '/js/lib/*.js',
 	scss: src + '/scss/*.scss',
 	html: src + '/**/*.html'
 };
@@ -23,7 +25,7 @@ gulp.task('server', function () {
 
 // 자바스크립트 파일을 하나로 합치고 압축한다.
 gulp.task('combine-js', function () {
-	return gulp.src(paths.js)
+	return gulp.src([paths.js, paths.js_lib])
 		.pipe(concat('script.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest(dist + '/js'));
@@ -33,13 +35,14 @@ gulp.task('combine-js', function () {
 gulp.task('compile-sass', function () {
 	return gulp.src(paths.scss)
 		.pipe(sass())
+		.pipe(cleancss())
 		.pipe(gulp.dest(dist + '/css'));
 });
 
 // HTML 파일을 압축한다.
 gulp.task('compress-html', function () {
 	return gulp.src(paths.html)
-		.pipe(minifyhtml())
+		//.pipe(minifyhtml())
 		.pipe(gulp.dest(dist + '/'));
 });
 
