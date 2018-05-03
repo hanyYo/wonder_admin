c5a21f45d37dd03bb7d90cda26b851c4793f080b
yjw <yjw@wemakeprice.com>
2018-05-03 14:47:25 +0900

    sample

---
+++ public/src/js/common.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $(".lnb>ul>li>a").on("click",function(){
+    $(".lnb .sec").not($(this).siblings("ul")).slideUp(300);
+    $(this).siblings("ul").slideToggle(300);
+  })
+})
