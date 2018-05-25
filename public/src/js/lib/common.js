3bddecdd8a06e322ddfd21313863d307b1274023
Lee Yo Han <ayoon486@gmail.com>
2018-05-25 15:31:12 +0900

    #이요한 - 레이아웃 세팅

--- public/src/js/lib/common.js
+++
@@ -1,7 +0,0 @@
-console.log('test2');
-$(document).ready(function(){
-  $(".lnb>ul>li>a").on("click",function(){
-    $(".lnb .sec").not($(this).siblings("ul")).slideUp(300);
-    $(this).siblings("ul").slideToggle(300);
-  })
-})
